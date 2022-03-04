4260ec29198272be55c1e4814d523c59455bec39
loekTheDreamer <luke.carelsen@me.com>
2022-03-04 09:28:39 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom'
+import { Suspense } from 'react'
+import './styles.css'
+import App from './App'
+
+ReactDOM.render(
+  <Suspense fallback={null}>
+    <App />
+  </Suspense>,
+  document.getElementById('root')
+)
